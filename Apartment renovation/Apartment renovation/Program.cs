3e3dd25757fa67fc2750b763992513b62305c4c1
Shazil0204 <141632242+Shazil0204@users.noreply.github.com>
2023-09-22 09:49:36 +0200

    Created
    a Visual studio program for the task

---
+++ Apartment renovation/Apartment renovation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apartment_renovation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
